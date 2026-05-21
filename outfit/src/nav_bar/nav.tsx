c646d87420637ae879e7162e3d814506a33cc5dd
Nabeel <nabeelpasha74@gmail.com>
2026-05-22 01:39:00 +0530

    themes added

--- outfit/src/nav_bar/nav.tsx
+++ outfit/src/nav_bar/nav.tsx
@@ -1,15 +1,21 @@
 import {motion} from "motion/react";
+import { useState } from "react";
 
 export default function NavBar(){
+    const [theme,setTheme]= useState("#FF0000");
+    function handleSubmit(theme : string){
+        setTheme(theme);
+    }
+    
     return(
         <>
-            <motion.div className="flex flex-row absolute opacity-100 w-screen h-10vh">
-                <div>++</div>
-                <a>shop</a>
-                <a>bag</a>
-                <button className="rounded-full w-5 h-5 bg-[#000000]"></button>
-                <button className="rounded-full w-5 h-5 bg-[#ECE9E4] border-2"></button>
-                <button className="rounded-full w-5 h-5 bg-[#FF0000]"></button>
+            <motion.div className="font-outfit text-4xl font-black flex flex-row absolute opacity-100 w-screen h-10vh">
+                <div className="text-6xl font-[1200]" style={{color : theme}}>++</div>
+                <a className="font-semibold" style={{color : theme}}>shop</a>
+                <a className="font-semibold" style={{color : theme}}>bag</a>
+                <button className="rounded-full w-5 h-5 bg-[#000000]" onClick={() => handleSubmit("#000000")}></button>
+                <button className="rounded-full w-5 h-5 bg-[#ECE9E4] border-2" onClick={() => handleSubmit("#ECE9E4")}></button>
+                <button className="rounded-full w-5 h-5 bg-[#FF0000]" onClick={() => handleSubmit("#FF0000")}></button>
             </motion.div>
         </>
     );
