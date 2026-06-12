d8d2f93e8f3828e0ef7081a50b1b2bd20a10561b
Nabeel <nabeelpasha74@gmail.com>
2026-06-12 22:36:10 +0530

    images added and css added

--- outfit/src/home_page/home.tsx
+++ outfit/src/home_page/home.tsx
@@ -1,23 +1,72 @@
-import { motion } from "motion/react"
+import { easeInOut, motion, px } from "motion/react"
+import image2 from "../assests/1.avif"
+import image3 from "../assests/2.avif"
+import image4 from "../assests/3.avif"
+import image1 from "../assests/4.avif"
 
-type homeProps={
-    theme:string;
+type homeProps = {
+    theme: string;
 }
-export default function Home({theme} : homeProps) {
-    return(
-        <>
-            <motion.div className={`${theme==="#000000" ? "bg-[#000000]" : "bg-[#ECE9E4]"} w-screen h-screen mt-0 pt-0 p-5`}>
-                <div className={`grid grid-flow-col font-bold font-outfit text-[27rem] tracking-tight ${theme==="#FF0000" ? "text-[#FF0000]" : theme==="#000000" ? "text-[#ECE9E4]" : "text-[#000000]"}  leading-none pt-7`}>
-                    <motion.p>O</motion.p>
-                    <motion.p>U</motion.p>
-                    <motion.p>T</motion.p>
-                    <motion.p>F</motion.p>
-                    <motion.p>I</motion.p>
-                    <motion.p>T</motion.p>
-                </div>
-                <motion.div className={`w-full h-2    ${theme==="#FF0000" ? "bg-[#FF0000]" : theme==="#000000" ? "bg-[#ECE9E4]" : "bg-[#000000]"} `}/>
+export default function Home({ theme }: homeProps) {
+    const text = "O U T F I T";
+    const parentProps = {
+        hidden: {},
+        show: {
+            transistion: {
+                staggerChildren: 2
+            }
+        }
+    }
 
-                
+    const childProps = {
+        hidden: {},
+        show: {}
+    }
+
+    return (
+        <>
+            <motion.div initial={{ y: 1000 }} animate={{ y: 0 }} transition={{ duration: 1.5, ease: easeInOut }} className={`${theme === "#000000" ? "bg-[#000000]" : "bg-[#ECE9E4]"} w-screen h-fill mt-0 pt-0 p-5`}>
+                <div className={`grid grid-flow-col font-bold font-outfit text-[27rem] tracking-tight ${theme === "#FF0000" ? "text-[#FF0000]" : theme === "#000000" ? "text-[#ECE9E4]" : "text-[#000000]"}  leading-none pt-7`}>
+                    <div>
+                        <motion.p variants={parentProps} initial="hidden" animate="show">
+                            {text.split(" ").map((char, index) => (
+                                <span key={index}  >
+
+                                    {char}
+                                </span>
+
+                            ))}
+                        </motion.p>
+                    </div>
+                </div>
+                <motion.div className={`w-full h-2    ${theme === "#FF0000" ? "bg-[#FF0000]" : theme === "#000000" ? "bg-[#ECE9E4]" : "bg-[#000000]"} `} />
+                <motion.div className={`grid font-outfit font-medium gap-70 grid-flow-col pt-8 ${theme === "#FF0000" ? "text-[#FF0000]" : theme === "#000000" ? "text-[#ECE9E4]" : "text-[#000000]"}`}>
+                    <div className="pr-10">OUTFIT</div>
+                    <div className="col-span-2 mr-10">
+                        WHY
+                        <p className=" tracking-tighter pt-5">Created by the ++hellohello team, this store and signature collection celebrates our collective creativity and passion for apparel. Carefully designed.</p>
+                    </div>
+                    <div className="grid  text-nowrap -mr-40 ">
+                        <span>Visit ++ website</span>
+                        <a>Shipping & Returns</a>
+                    </div>
+                    <div className="pl-10 text-nowrap mr-5">© 2026</div>
+                </motion.div>
+                <motion.div className="flex justify-around pt-10 ">
+                    <button className="hover:">
+                        <img src={image1} className="w-85 h-113.5" ></img>
+                    </button>
+                    <button>
+                        <img src={image2} className="w-85 h-113.5" ></img>
+                    </button>
+                    <button>
+                        <img src={image3} className="w-85 h-113.5"></img>
+                    </button>
+                    <button>
+                        <img src={image4} className="w-85 h-113.5" ></img>
+                    </button>
+                </motion.div>
+
             </motion.div>
         </>
     );
